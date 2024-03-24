634a7930ac6e5f4f2463ff164e99c9e66b526514
Ardian <97308816+ArdiannS@users.noreply.github.com>
2024-03-24 13:27:05 +0100

    Update read_csv.js

--- read_csv.js
+++ read_csv.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 const csv = require('csv-parser');
 
 // Path to your CSV file
-const csvFilePath = 'https://raw.githubusercontent.com/cluemediator/read-large-csv-nodejs/master/product_10L.csv';
+const csvFilePath = 'Sample-Spreadsheet-100-rows.csv';
 
 // Read data from CSV file
 fs.createReadStream(csvFilePath)
