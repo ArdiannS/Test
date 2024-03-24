83f145d99152ad84fe91c41f648adb3212234749
ArdiannS <ardiannsutaj@gmail.com>
2024-03-24 13:59:18 +0100

    change

--- read_csv.js
+++ read_csv.js
@@ -3,11 +3,37 @@ const csv = require("csv-parser");
 
 // Path to your CSV file
 const csvFilePath = "Sample-Spreadsheet-100-rows.csv";
-
+const token = process.env.GITHUB_TOKEN;
+console.log(token);
 // Read data from CSV file
 fs.createReadStream(csvFilePath)
   .pipe(csv())
-  .on("data", (row) => {
+  .on("data", async (row) => {
+    const url = `https://api.github.com/repos/ArdiannS/CareConnect/collaborators/albin-sh`;
+    try {
+      const response = await fetch(url, {
+        method: "DELETE",
+        headers: {
+          Authorization: `token ${token}`,
+          Accept: "application/vnd.github.v3+json",
+          "Content-Type": "application/json",
+        },
+      });
+
+      if (response.ok) {
+        console.log(
+          `User ${username} removed from the repository ${owner}/${repo}.`
+        );
+      } else {
+        console.error(
+          `Failed to remove user ${username} from the repository ${owner}/${repo}.`
+        );
+        const responseData = await response.json();
+        console.error("Error message:", responseData.message);
+      }
+    } catch (error) {
+      console.error("Error occurred while making the request:", error.message);
+    }
     // Process each row of data
     console.log(row.ID);
     console.log(row.Name);
